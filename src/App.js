bb1ce8d4968e406726644047701b4e321df4622b
deanxx0 <74574123+deanxx0@users.noreply.github.com>
2020-12-06 12:07:34 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
+// import { computeHeadingLevel } from "@testing-library/react";
 import React from "react";
 
 class App extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {flag : true};
+  //   this.handleOnClick = this.handleOnClick.bind(this);
+  // }
+  
   state = {
-    count: 0
-  };
-  add = () => {
-    this.setState(current => ({ count: current.count + 1 }));
-  };
-  minus = () => {
-    this.setState(current => ({ count: current.count - 1 }));
-  };
+    flag: true
+  }
+
+  handleOnClick = () => {
+    if(this.state.flag === true) {
+      this.setState({flag: false});
+    } else {
+      this.setState({flag: true});
+    }
+  }
+
   render() {
-    return (
-      <div>
-        <h3>the number: {this.state.count}</h3>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
+    if (this.state.flag === true) {
+      return <button onClick={this.handleOnClick}>log out</button>
+    } else {
+      return <button onClick={this.handleOnClick}>log in</button>
+    }
   }
 }
 
