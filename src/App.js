a6ec9310ec4a41ee7ee0ec47c2a0d499fccdf3e6
deanxx0 <74574123+deanxx0@users.noreply.github.com>
2020-12-20 10:13:44 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,33 +1,49 @@
 // import { computeHeadingLevel } from "@testing-library/react";
 import React from "react";
 
+class Components01 extends React.Component {
+  render() {
+    return (
+      <div>sub components 1.</div>
+    )
+  }
+}
+class Components02 extends React.Component {
+  render() {
+    return (
+      <div>sub components 2.</div>
+    )
+  }
+}
+
 class App extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {flag : true};
-  //   this.handleOnClick = this.handleOnClick.bind(this);
-  // }
+  constructor(props) {
+    super(props);
+    this.state = {flag : true};
+  }
   
   state = {
     flag: true
   }
 
   handleOnClick = () => {
-    if(this.state.flag === true) {
+    if(this.state.flag) {
       this.setState({flag: false});
     } else {
       this.setState({flag: true});
     }
+
   }
 
   render() {
-    if (this.state.flag === true) {
-      return <button onClick={this.handleOnClick}>log out</button>
+    if (this.state.flag) {
+      return <button onClick={this.handleOnClick}><Components01/></button>
     } else {
-      return <button onClick={this.handleOnClick}>log in</button>
+      return <button onClick={this.handleOnClick}><Components02/></button>
     }
   }
 }
+
 
 export default App;
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,83 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+
+const scaleNames = {
+  c: 'Celsius',
+  f: 'Fahrenheit'
+};
+
+// eslint-disable-next-line
+function toCelsius(fahrenheit) {
+  return (fahrenheit - 32) * 5 / 9;
+}
+// eslint-disable-next-line
+function toFahrenheit(celsius) {
+  return (celsius * 9 / 5) + 32;
+}
+// eslint-disable-next-line
+function tryConvert(temperature, convert) {
+  const input = parseFloat(temperature);
+  if (Number.isNaN(input)) {
+    return '';
+  }
+  const output = convert(input);
+  const rounded = Math.round(output * 1000) / 1000;
+  return rounded.toString();
+}
+class TemperatureInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.props.fromParent(e.target.value)
+  }
+
+  render() {
+    const temperature = this.props.temperature;
+    const scale = this.props.scale;
+    return (
+      <fieldset>
+        <legend>Enter temperature in {scaleNames[scale]}:</legend>
+        <input value={temperature}
+               onChange={this.handleChange} />
+      </fieldset>
+    );
+  }
+}
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      temperatureC: '',
+      temperatureF: ''
+    }
+  }
+
+  onTemperatureCChange = (value) => {
+    console.log('on temperatureC : ',value)
+    this.setState({temperatureC: value, temperatureF: toFahrenheit(value)});
+  }
+
+  onTemperatureFChange = (value) => {
+    console.log('on temperatureF : ',value)
+    this.setState({temperatureC: toCelsius(value), temperatureF: value});
+    
+  }
+
+  render() {
+    return (
+      <div>
+        <TemperatureInput scale="c" fromParent={this.onTemperatureCChange} temperature={this.state.temperatureC}/>
+        <TemperatureInput scale="f" fromParent={this.onTemperatureFChange} temperature={this.state.temperatureF}/>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(
-  <React.StrictMode><App /></React.StrictMode>,
+  <Calculator />,
   document.getElementById('root')
 );
-
